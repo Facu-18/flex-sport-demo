af1c06132734803fcd8d522c6385b7e418fe466e
Facundo <facundomoriconi19@gmail.com>
2026-03-17 06:37:54 -0300

    commit

--- src/components/layout/WhatsAppBadge.tsx
+++ src/components/layout/WhatsAppBadge.tsx
@@ -6,7 +6,7 @@ export function WhatsAppBadge() {
       aria-hidden="true"
       className="pointer-events-none fixed bottom-5 right-5 z-40 flex h-14 w-14 items-center justify-center rounded-full bg-[#25D366] shadow-xl shadow-black/25 ring-2 ring-white"
     >
-      <Image src="/wsp.png" alt="" width={30} height={30} className="h-7 w-7" />
+      <Image src="/wsp.svg" alt="" width={30} height={30} className="h-7 w-7" />
     </div>
   );
 }
